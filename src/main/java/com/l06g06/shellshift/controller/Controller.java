0cef2de9e9c4911258c47455d5ae3bf5be04da35
vanessa-sbq <up202207919@up.pt>
2023-11-23 12:38:47 +0000

    Started implementing Controller superclass

--- src/main/java/com/l06g06/shellshift/controller/Controller.java
+++ src/main/java/com/l06g06/shellshift/controller/Controller.java
@@ -1,4 +1,15 @@
 package com.l06g06.shellshift.controller;
 
-public abstract class Controller {
+public abstract class Controller<T> {
+    private final T model;
+
+    public Controller(T model){
+        this.model = model;
+    }
+
+    public T getModel() {
+        return model;
+    }
+
+    //public abstract void step();
 }
