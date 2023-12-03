118455986720552ecab8bc5af1da41270158e7ec
vanessa-sbq <up202207919@up.pt>
2023-12-03 11:43:23 +0000

    Started implementing Coin MVC

--- src/main/java/com/l06g06/shellshift/viewer/game/CoinViewer.java
+++ src/main/java/com/l06g06/shellshift/viewer/game/CoinViewer.java
@@ -4,6 +4,19 @@ import com.l06g06.shellshift.gui.Gui;
 import com.l06g06.shellshift.model.game.elements.Coin;
 
 public class CoinViewer implements ElementViewer<Coin>{
+
+    /*private Coin coin;  // Corresponding Model
+
+    public static final String[] COIN={
+        " AA ",
+        "AAAA",
+        "AAAA",
+        "AAAA",
+        "AAAA",
+        "AAAA",
+        " AA "
+    };*/
+
     @Override
     public void draw(Coin coin, Gui gui){
         gui.drawCoin(coin.getPosition());
