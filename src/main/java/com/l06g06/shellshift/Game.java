f3b110c876c4e09e774307688822c3ed6167a9dc
AntonioAbilio <up202205469@up.pt>
2023-12-05 16:04:25 +0000

    Solved some conflicts.

--- src/main/java/com/l06g06/shellshift/Game.java
+++ src/main/java/com/l06g06/shellshift/Game.java
@@ -37,7 +37,7 @@ public class Game {
     }
 
     private void start() throws IOException {
-        int FPS = 10;
+        int FPS = 60;
         int frameTime = 1000 / FPS;
         this.keysListen = new ListenForKeys(this.gui);
 
