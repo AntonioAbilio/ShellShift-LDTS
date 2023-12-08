0c543b215c3b62e0fdf62141502b564547998383
vanessa-sbq <up202207919@up.pt>
2023-12-08 17:46:49 +0000

    VerticalMoveStrategy now implemented

--- src/main/java/com/l06g06/shellshift/model/game/elements/enemies/moveStrategies/VerticalMoveStrategy.java
+++ src/main/java/com/l06g06/shellshift/model/game/elements/enemies/moveStrategies/VerticalMoveStrategy.java
@@ -3,8 +3,30 @@ package com.l06g06.shellshift.model.game.elements.enemies.moveStrategies;
 import com.l06g06.shellshift.model.game.elements.Position;
 
 public class VerticalMoveStrategy implements MoveStrategy{
+
+    private int distToTop;
+    private boolean movingUp;
+
+    public VerticalMoveStrategy(){
+        this.distToTop = 0;
+        this.movingUp = true;
+    }
+
     @Override
     public Position move(Position currentPos) {
-        return currentPos;
+        if (movingUp) {
+            distToTop++;
+            if (distToTop >= 20){
+                movingUp = false;
+            }
+            return new Position(currentPos.getX(), currentPos.getY() - 1);
+        } else {
+            distToTop--;
+            if (distToTop <= 0){
+                movingUp = true;
+            }
+            return new Position(currentPos.getX(), currentPos.getY() + 1);
+        }
     }
+
 }
