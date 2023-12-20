dd4a4ee8b6c8ca55f050c733108499d459c361c4
vcosta <costavasco03@gmail.com>
2023-12-20 16:51:37 +0000

    now really completed

--- src/test/java/com/l06g06/shellshift/viewers/TutorialViewerTest.java
+++ src/test/java/com/l06g06/shellshift/viewers/TutorialViewerTest.java
@@ -2,10 +2,12 @@ package com.l06g06.shellshift.viewers;
 
 import com.l06g06.shellshift.Components;
 import com.l06g06.shellshift.gui.Gui;
-import com.l06g06.shellshift.model.game.elements.Chell;
-import com.l06g06.shellshift.model.game.elements.Cloud;
-import com.l06g06.shellshift.model.game.elements.Platform;
-import com.l06g06.shellshift.model.game.elements.Position;
+import com.l06g06.shellshift.model.game.elements.*;
+import com.l06g06.shellshift.model.game.elements.enemies.Enemy;
+import com.l06g06.shellshift.model.game.elements.enemies.HardMonster;
+import com.l06g06.shellshift.model.game.elements.enemies.SoftMonster;
+import com.l06g06.shellshift.model.game.elements.enemies.moveStrategies.HorizontalMoveStrategy;
+import com.l06g06.shellshift.model.game.elements.enemies.moveStrategies.VerticalMoveStrategy;
 import com.l06g06.shellshift.model.game.gun.Gun;
 import com.l06g06.shellshift.model.game.gun.NormalFireStrategy;
 import com.l06g06.shellshift.model.tutorial.TutorialMap;
@@ -30,10 +32,7 @@ public class TutorialViewerTest {
         tutorial = Mockito.mock(TutorialMap.class);
         tutorialViewer = new TutorialViewer(tutorial);
         gui = Mockito.mock(Gui.class);
-    }
 
-    @Test
-    void drawElementsSelected() {
         Position pos = new Position(0,0);
         Chell chell = new Chell(pos);
         Platform platform = new Platform(pos);
@@ -43,9 +42,42 @@ public class TutorialViewerTest {
         when(tutorial.getChell()).thenReturn(chell);
         when(tutorial.getGun()).thenReturn(gun);
         when(tutorial.getPlatform()).thenReturn(platform);
-        when(tutorial.getBullets()).thenReturn(Collections.emptyList());
-        when(tutorial.getCoins()).thenReturn(Collections.emptyList());
-        when(tutorial.getEnemies()).thenReturn(Collections.emptyList());
+    }
+
+    @Test
+    void drawHUD() {
+        tutorialViewer.drawElements(gui);
+
+        verify(gui, times(1)).setBackground(Mockito.any(String.class));
+        verify(gui, times(1)).drawImageASCII(eq(Components.Chell.getImage()), Mockito.any(Position.class));
+        verify(gui, times(1)).drawImageASCII(eq(Components.Platform.getImage()), Mockito.any(Position.class));
+
+        verify(gui, times(1)).drawImageASCII(eq(Components.Bullet.getImage()), Mockito.any(Position.class));
+        verify(gui, times(1)).numToASCII(eq(tutorial.getGun().getNumBullets()), anyInt(), anyInt());
+
+        verify(gui, times(1)).drawImageASCII(eq(Components.Coin.getImageSelected()), Mockito.any(Position.class));
+        verify(gui, times(1)).numToASCII(eq(tutorial.getCoinsCollected()), anyInt(), anyInt());
+    }
+
+    @Test
+    void drawKeysNonSelected() {
+        when(tutorial.isSelectedArrowUp()).thenReturn(false);
+        when(tutorial.isSelectedArrowLeft()).thenReturn(false);
+        when(tutorial.isSelectedArrowRight()).thenReturn(false);
+        when(tutorial.isSelectedSpace()).thenReturn(false);
+
+        tutorialViewer.drawElements(gui);
+
+
+        verify(gui, times(1)).drawImageASCII(eq(Components.ArrowUp.getImage()), Mockito.any(Position.class));
+        verify(gui, times(1)).drawImageASCII(eq(Components.ArrowLeft.getImage()), Mockito.any(Position.class));
+        verify(gui, times(1)).drawImageASCII(eq(Components.ArrowRight.getImage()), Mockito.any(Position.class));
+        verify(gui, times(1)).drawImageASCII(eq(Components.SpaceBar.getImage()), Mockito.any(Position.class));
+
+    }
+
+    @Test
+    void drawKeysSelected() {
         when(tutorial.isSelectedArrowUp()).thenReturn(true);
         when(tutorial.isSelectedArrowLeft()).thenReturn(true);
         when(tutorial.isSelectedArrowRight()).thenReturn(true);
@@ -53,16 +85,9 @@ public class TutorialViewerTest {
 
         tutorialViewer.drawElements(gui);
 
-        verify(gui, times(1)).setBackground(Mockito.any(String.class));
-        verify(gui, times(1)).drawImageASCII(eq(Components.Chell.getImage()), Mockito.any(Position.class));
-        verify(gui, times(1)).drawImageASCII(eq(Components.Platform.getImage()), Mockito.any(Position.class));
         verify(gui, times(1)).drawImageASCII(eq(Components.ArrowUp.getImageSelected()), Mockito.any(Position.class));
         verify(gui, times(1)).drawImageASCII(eq(Components.ArrowLeft.getImageSelected()), Mockito.any(Position.class));
         verify(gui, times(1)).drawImageASCII(eq(Components.ArrowRight.getImageSelected()), Mockito.any(Position.class));
         verify(gui, times(1)).drawImageASCII(eq(Components.SpaceBar.getImageSelected()), Mockito.any(Position.class));
-        verify(gui, times(1)).drawImageASCII(eq(Components.Bullet.getImage()), Mockito.any(Position.class));
-        verify(gui, times(1)).numToASCII(eq(tutorial.getGun().getNumBullets()), anyInt(), anyInt());
-        verify(gui, times(1)).drawImageASCII(eq(Components.Coin.getImageSelected()), Mockito.any(Position.class));
-        verify(gui, times(1)).numToASCII(eq(tutorial.getCoinsCollected()), anyInt(), anyInt());
     }
 }
